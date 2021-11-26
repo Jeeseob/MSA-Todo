c5126d94696e6d28bb9c181b92d680ae444f4320
Jeeseob <jeeseob5761@gmail.com>
2021-11-26 20:59:45 +0900

    test

--- week12/6-2.c
+++ week12/6-2.c
@@ -10,18 +10,19 @@ int main(void)
 	for (int i = 0; i<3; i++) {
 		if((pid = fork()) < 0)
 		{
-			//fork를 출력하고 종료
+			//자식 프로세스 생성 중 오류 발생시 에러 메세지 출력 및 료
 			perror("fork");
 			exit(1);
 		}
 
-		// 그렇지않고 pid 가 양수이면 즉 부모 프로세스이면
+		// pid가 양수이면, 부모 프로세스이기 때문에, 자식 프로세스 일때만 동작.
 		if(pid <= 0)
 		{
 			printf("my pid is %d ppid is %d\n", (int)getpid()
 				, (int)getppid());
-			
+			// pid, ppid 출력 후 종료
 			return 0;
 		}	
 	}
+	return 0;
 }

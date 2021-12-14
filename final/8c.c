fdeaf1fee75542d2298aaa57b43c4c077c9865d2
Jeeseob <jeeseob5761@gmail.com>
2021-12-14 15:32:59 +0900

    test

--- final/8c.c
+++ final/8c.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 
 
 
-    	buf = "test";
+    	sprintf(buf, "bufTEST");
     	if (send(sd, buf, strlen(buf) + 1, 0) == -1) {
 	        perror("send");
 	        exit(1);

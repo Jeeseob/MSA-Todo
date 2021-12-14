f3c48bb1695edb14e1625e8039c7a58abd0b4f91
Jeeseob <jeeseob5761@gmail.com>
2021-12-14 11:11:24 +0900

    test

--- final/5.c
+++ final/5.c
@@ -1,4 +1,46 @@
-// 5.c
 
-sigactioin() 함수를 사용해서 수정, Ctrl-c를 눌러도 중단되지 않고,
-Ctrl -\ 를 윕력하여야 중단되도록 
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void handler(int signo) 
+{
+    	psignal(signo, "Received Signal:");
+    	sleep(5);
+    	printf("In Signal Handler, After Sleep\n");
+}
+
+
+int mina(void) {
+
+
+	struct sigaction act;
+
+    	sigemptyset(&act.sa_mask);
+    	sigaddset(&act.sa_mask, SIGQUIT);
+    	act.sa_flags = 0;
+    	act.sa_handler = handler;
+    	if (sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0) 
+	{
+        	perror("sigaction");
+        	exit(1);
+    	}
+
+    	fprintf(stderr, "Input SIGINT: ");
+    	pause();
+    	fprintf(stderr, "After Signal Handler\n");
+
+    	return 0;
+
+
+	// short i = 0;
+
+	// while(1) {
+	// 		if(i<0)
+	// 			i = 0;
+	// 		printf("%d\r",i++);
+	// }
+	// return 0;
+}

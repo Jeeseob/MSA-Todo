1e6a4f0116ad675b598875207d6dbb02f058a56f
Jeeseob <jeeseob5761@gmail.com>
2021-12-14 11:21:47 +0900

    test

--- final/5.c
+++ final/5.c
@@ -16,25 +16,26 @@ void handler(int signo)
 int mina(void) {
 
 
-
 	struct sigaction act;
 
-    	sigemptyset(&act.sa_mask);
-    	sigaddset(&act.sa_mask, SIGQUIT);
-    	act.sa_flags = 0;
-    	act.sa_handler = handler;
-    	if (sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0) 
-	{
+    sigemptyset(&act.sa_mask);
+    // sigaddset(&act.sa_mask, SIGQUIT);
+    // act.sa_flags = 0;
+    // act.sa_handler = handler;
+
+    if (sigaction(SIGINT, &act, (struct sigaction *)NULL) < 0) {
         	perror("sigaction");
         	exit(1);
-    	}
+    }
 
 	short i = 0;
 
 	while(1) {
-			if(i<0)
-				i = 0;
-			printf("%d\r",i++);
+		if(i<0)
+		i = 0;
+		printf("%d\r",i++);
 	}
+
 	return 0;
 }
+

83146531e551e12a31d51a73b328d0e959258f19
Jeeseob <jeeseob5761@gmail.com>
2021-12-14 10:18:24 +0900

    test

--- final/test3.c
+++ final/test3.c
@@ -2,13 +2,6 @@
 #include <pthread.h>
 #include <stdlib.h>
 
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-
-#include <unistd.h>
-
 
 #define N 100000000
 

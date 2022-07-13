6d1435d71497d48b8ee9ae58efec805c523703a3
Jeeseob <jeeseob5761@gmail.com>
2022-07-13 19:09:30 +0900

    N과M(3)

---
+++ src/July13th/Baek15649/Main.java
@@ -0,0 +1,54 @@
+package July13th.Baek15649;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/07/13
+ * @Problem : N과 M(1) : https://www.acmicpc.net/problem/15651
+ */
+
+public class Main {
+    private static boolean[] visited;
+    private static int[] answer;
+    private static int N;
+    private static int M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        // 정답처리용 Array
+        answer = new int[M];
+
+        // 방문체크용 Array 초기화
+        visited = new boolean[N+1];
+        Arrays.fill(visited,false);
+
+        dfs(0);
+    }
+    public static void dfs(int depth) throws IOException {
+        if (depth == M) {
+            for (int temp : answer) {
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        // 방문한적 없는 index 중 가장 먼저 처리
+        for (int i = 1; i <= N; i++) {
+            if(!visited[i]) {
+                visited[i] = true;
+                answer[depth] = i;
+                dfs(depth + 1);
+
+                visited[i] = false; // 다음 연산을 위해 초기화
+            }
+        }
+    }
+}
--- src/July13th/Baek15651/Main.java
+++ src/July13th/Baek15651/Main.java
@@ -1,20 +1,19 @@
-package July13th.BaekJoon15649;
+package July13th.Baek15651;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/07/13
- * @Problem : N과 M(1) : https://www.acmicpc.net/problem/15651
+ * @Problem : N과 M(3) : https://www.acmicpc.net/problem/15651
  */
 
 public class Main {
-    private static boolean[] visited;
     private static int[] answer;
     private static int N;
     private static int M;
+    private static StringBuilder stringBuilder;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,30 +24,23 @@ public class Main {
         // 정답처리용 Array
         answer = new int[M];
 
-        // 방문체크용 Array 초기화
-        visited = new boolean[N+1];
-        Arrays.fill(visited,false);
-
+        stringBuilder = new StringBuilder();
         dfs(0);
+        System.out.println(stringBuilder.toString());
     }
     public static void dfs(int depth) throws IOException {
         if (depth == M) {
             for (int temp : answer) {
-                System.out.print(temp + " ");
+                stringBuilder.append(temp).append(" ");
             }
-            System.out.println();
+            stringBuilder.append('\n');
             return;
         }
 
         // 방문한적 없는 index 중 가장 먼저 처리
         for (int i = 1; i <= N; i++) {
-            if(!visited[i]) {
-                visited[i] = true;
                 answer[depth] = i;
                 dfs(depth + 1);
-
-                visited[i] = false; // 다음 연산을 위해 초기화
-            }
         }
     }
 }

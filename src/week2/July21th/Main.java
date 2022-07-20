226848aa99fcd643b41fa8c8058fb448c44ed8d9
Jeeseob <jeeseob5761@gmail.com>
2022-07-20 18:41:02 +0900

    백준 상자넣기 문제

--- src/week2/July21th/Main.java
+++ src/week2/July21th/Main.java
@@ -1,18 +1,39 @@
-package week2.July18th;
+package week2.July21th;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/07/15
- * @Problem : 괄호 : https://www.acmicpc.net/problem/9012
+ * @Problem : 상자넣기 : https://www.acmicpc.net/problem/1965
  */
 
 public class Main {
-    public Main() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
+        int[] boxes = new int[N];
+
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            boxes[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        int[] numbers = new int[N];
+        Arrays.fill(numbers, 1);
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (boxes[j] < boxes[i] && numbers[j] >=  numbers[i]) {
+                    numbers[i] = numbers[j] + 1;
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(numbers).max().orElse(1));
     }
 }
--- src/week2/July22th/Main.java
+++ src/week2/July22th/Main.java
@@ -1,15 +1,15 @@
-package week2.July18th;
+package week2.July22th;
 
 import java.io.*;
 
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/07/15
- * @Problem : 괄호 : https://www.acmicpc.net/problem/9012
+ * @Problem : 문제 : https://www.acmicpc.net/problem/
  */
 
 public class Main {
-    public Main() throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 

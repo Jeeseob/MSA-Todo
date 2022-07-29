48a2eff8bb903a4dff890704ef4fd45fcbe7306d
Jeeseob <jeeseob5761@gmail.com>
2022-07-29 11:21:51 +0900

    problem init

--- src/week4/August1st/Main.java
+++ src/week4/August1st/Main.java
@@ -1,5 +1,22 @@
-package week4.August1st;/**
+package week4.August1st;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
  * @Author : Jeeseob
  * @CreateAt : 2022/08/01
- */public class Main {
+ * @Problem : DFS와 BFS : https://www.acmicpc.net/problem/1260
+ */
+
+public class Main {
+
+    private static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    }
 }

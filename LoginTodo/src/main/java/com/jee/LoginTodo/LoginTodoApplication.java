db91a641e38e349d5eb74ba3335841fe3e4b4fe5
Jeeseob <jeeseob5761@gmail.com>
2022-09-28 21:02:57 +0900

    git ignore 설정

---
+++ LoginTodo/src/main/java/com/jee/LoginTodo/LoginTodoApplication.java
@@ -0,0 +1,13 @@
+package com.jee.LoginTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginTodoApplication.class, args);
+	}
+
+}
